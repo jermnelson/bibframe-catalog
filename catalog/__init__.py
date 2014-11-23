475bab8e29a047fe39a33547a1028208149bbc74
Jeremy Nelson <jermnelson@gmail.com>
2014-11-23 08:34:35 -0700

    Added html view for results, continued to debug recursive ingestion

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -14,14 +14,32 @@ __license__ = "GPLv3"
 
 
 from flask import abort, Flask, jsonify, render_template, redirect, request
-from flask.ext.elastic import Elastic
+#from flask.ext.elastic import Elastic
+from elasticsearch import Elasticsearch
 import sys
-sys.path.append("C:\\Users\\jernelson\\Development\\flask-fedora")
+#sys.path.append("C:\\Users\\jernelson\\Development\\flask-fedora")
 from flask_fedora_commons import Repository
 
 app = Flask(__name__)
 repository = Repository(app)
-es_search = Elastic(app)
+#es_search = Elastic(app)
+es_search = Elasticsearch()
+
+@app.template_filter('name')
+def guess_name(entity):
+    """Name filter attempts to serialize a BIBFRAME entity"""
+    name = ''
+    if 'bf:titleValue' in entity:
+        name = ','.join(entity.get('bf:titleValue'))
+    if 'bf:subtitle' in entity:
+        name = ','.join(entity.get('bf:subtitle'))
+    elif 'bf:titleStatement' in entity:
+        name = ','.join(entity.get('bf:titleStatement'))
+    elif 'bf:workTitle' in entity:
+        name = ','.join([guess_name(title) for title in entity.get('bf:workTitle')])
+    elif 'bf:label' in entity:
+        name = ','.join(entity.get('bf:label'))
+    return name
 
 @app.route('/search', methods=['POST', 'GET'])
 def search():
@@ -35,7 +53,16 @@ def search():
             q=phrase,
             index='bibframe',
             doc_type=search_type)
-    return jsonify(result)
+    for hit in result.get('hits').get('hits'):
+        for key, value in hit['_source'].items():
+            if key.startswith('fcrepo:uuid'):
+                continue
+            for i,row in enumerate(value):
+                if es_search.exists(id=row, index='bibframe'):
+                    hit['_source'][key][i] = es_search.get_source(id=row, index='bibframe')
+
+    return render_template('results.html', search_type=search_type, result=result, phrase=phrase)
+    #return jsonify(result)
     #return "{} phrase={}".format(search_type, phrase)
 
 

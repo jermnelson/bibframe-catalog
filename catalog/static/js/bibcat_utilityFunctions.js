ad565211d84eb9ef0e62cda3018bb40fa250cbbc
Mike Stabile <mstabile75@gmail.com>
2015-08-18 05:55:41 -0600

    fixed tt link and testing of horizontal nav

--- catalog/static/js/bibcat_utilityFunctions.js
+++ catalog/static/js/bibcat_utilityFunctions.js
@@ -3,6 +3,7 @@ function isNotNull (variable) {
 	if(variable === null && typeof variable === "object"){var result = false}; //Null Test
 	if(variable === "" && typeof variable === "string"){var result = false}; //Empty String Test
 	if(variable === undefined && typeof variable === "undefined"){var result = false}; //Undefined Test
+	if(variable === "undefined"){var result = false}; //Undefined Test 2
 	if(variable === false && typeof variable === "boolean"){var result = false}; //False Test
 	if(variable === 0 && typeof variable === "number"){var result = false}; //Zero Test
 	if(!parseFloat(variable) && variable != 0 && typeof variable === "number") {var result = false}; //NaN Test
